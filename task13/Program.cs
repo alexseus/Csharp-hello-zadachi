b062dd5d0885401428293b1b82d40b9444d35306
aleksei <git@alexsei.ru>
2022-12-19 23:08:01 +0300

    task15

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+int TrirdDigit(int number)
+{
+    int digit3 = number % 10;
+    int digit1 = number / 100;
+    int digit2 = number / 10 - digit1*10;
+    return digit2;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
---
+++ task15/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+void WeekDay(int number)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите цифру от 1 до 7 обозначающую день недели: ");
+int weeknum = Convert.ToInt32(Console.ReadLine());
+if ( weeknum > 0 && weeknum < 8 )
+{
+    WeekDay(weeknum);
+}
+else
+{
+    Console.WriteLine("Введенное число вне диапазона");
+}
