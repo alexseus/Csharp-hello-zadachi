0aea37f0c9432a058fd212f6d638c2f6b9a219ed
aleksei <git@alexsei.ru>
2022-12-23 14:29:59 +0300

    task17 &rename

---
+++ task17/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+string getRangeFromQuarter(int quarter)
+{
+    string result = $"Диапазон значений в {quarter} четверти ";
+    switch(quarter)
+    {
+        case 1:
+        {
+            result += "x > 0 и y > 0";
+            break;
+        }
+        case 2:
+        {
+            result += "x < 0 и y > 0";
+            break;
+        }case 3:
+        {
+            result += "x < 0 и y < 0";
+            break;
+        }case 4:
+        {
+            result += "x > 0 и y < 0";
+            break;
+        }
+    }
+    return result;
+}
+
+int quarter = getUserValue("Введите номер четверти");
+string rangeDescription = getRangeFromQuarter(quarter);
+Console.WriteLine(rangeDescription);
