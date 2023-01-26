47c220e4f947c7dc3e02b0e32e650d9c4d961355
aleksei <git@alexsei.ru>
2023-01-26 09:45:39 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+/* int GetUserData(string message);
+{
+    Console.Wrtite(message);
+    int userdata = int.Parse(Console.ReadLine()!);
+    return userdata;
+}
+*/
+
+double[,] CreateArray(int strings, int columns)
+{
+    double[,] array = new double[strings, columns];
+    int size = 99;
+    for (int i = 0; i < strings; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(-size, size + 1) / (10 + 0.0);
+        }
+    }
+    return array;
+}
+
+void Show2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = CreateArray(3, 4);
+Show2DArray(array);
